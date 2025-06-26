8c15457c4941d2a581bfee5879f3c05e5c843606
lapylia <liliaory81@gmail.com>
2025-06-26 12:52:41 +0300

    мяу

--- PZ_14/PZ_14.py
+++ PZ_14/PZ_14.py
@@ -2,15 +2,29 @@
 # фамилий. Создать новый файл, в котором выполнить замену слова «роман» на слово
 # «произведение».
 import re
-surname = r'^([\w\-]+)'
-with open('writer.txt','r', encoding='utf-8') as file:
-    text = file.read()
 
-all_surnames = set(re.findall(surname, text, re.MULTILINE))
+surname = r'^([\w\-]+)'
+
+# Чтение файла
+try:
+    with open('C:/Users/lilia/OneDrive/Documents/GitHub/Python_Oru/PZ_14/writer.txt', 'r', encoding='utf-8') as file:
+        text = file.read()
+except FileNotFoundError:
+    print("Ошибка: writer.txt не найден.")
+    exit()
+
+# Извлечение фамилий
+all_surnames = set(re.findall(surname, text))
 count_surnames = len(all_surnames)
-print('Фамилии писателей: ', all_surnames)
-print('Количество фамилий: ', count_surnames)
+
+print('Фамилии писателей:', all_surnames)
+print('Количество фамилий:', count_surnames)
+
+# Замена "роман" на "произведение"
 new_words = text.replace('роман', 'произведение')
 
-with open('writer1.txt', 'w', encoding='utf-8') as new_file:
+# Запись в новый файл
+with open('C:/Users/lilia/OneDrive/Documents/GitHub/Python_Oru/PZ_14/writer1.txt', 'w', encoding='utf-8') as new_file:
     new_file.write(new_words)
+
+print("Изменения записаны в writer1.txt") 
